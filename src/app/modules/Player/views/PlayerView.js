d76acf4304df398c39e9a2e733d187f9f7c4fbe8
Fedor Nezhivoy <gyzerok@gmail.com>
2014-11-05 21:38:16 +0600

    commit

--- src/app/modules/Player/views/PlayerView.js
+++ src/app/modules/Player/views/PlayerView.js
@@ -2,6 +2,20 @@
 
 module.exports = function (PlayerApp) {
     return Backbone.Marionette.ItemView.extend({
-        template: PlayerApp.getTemplate('player')
+        template: PlayerApp.getTemplate('player'),
+        className: 'player-container',
+
+        ui: {
+            player: 'audio#player',
+            playBtn: 'div#play-btn',
+            prevBtn: 'div#prev-btn',
+            nextBtn: 'div#next-btn'
+        },
+
+        triggers: {
+            'click @ui.playBtn': 'player:play',
+            'click @ui.prevBtn': 'player:next',
+            'click @ui.nextBtn': 'player:prev'
+        }
     });
 };
